3ff46c8b0100cf47c94715477dac2637cedb9ffe
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-18 22:51:31 +0530

    Create subarraysum.cpp

---
+++ Programs/Array/subarraysum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> umap;
+        int ans = 0;
+        int sum = 0;
+        
+        for(int& num : nums) {
+            sum += num;
+            if(sum == k) ans++;
+            if(umap.find(sum - k) != umap.end()) ans += umap[sum - k];
+            
+            umap[sum]++;
+        }
+        
+        return ans;
+    }
+};
