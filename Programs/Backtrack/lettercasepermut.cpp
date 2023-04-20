3a10231d41366302d0f9ebabb4573126a3d91960
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-20 08:55:13 +0530

    Create lettercasepermut.cpp

---
+++ Programs/Backtrack/lettercasepermut.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<string> ans;
+public:
+    void backtrack(string& str, int i, string s) {
+        int n = str.length();
+        if(i >= n) {
+            ans.push_back(s);
+            return;
+        }
+        
+        if(isalpha(str[i])) {
+            char temp, ch = str[i];
+            if(isupper(ch)) temp = tolower(ch);
+            else temp = toupper(ch);
+            
+            backtrack(str, i + 1, s + temp);
+        }
+        
+        backtrack(str, i + 1, s + str[i]);
+    }
+    
+    vector<string> letterCasePermutation(string s) {
+        backtrack(s, 0, "");
+        
+        return ans;
+    }
+};
