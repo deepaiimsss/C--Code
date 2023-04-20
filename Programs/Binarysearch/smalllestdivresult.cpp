7af376927827f2f7c5af1763fb7865494170ef4d
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-20 09:43:09 +0530

    Create smalllestdivresult.cpp

---
+++ Programs/Binarysearch/smalllestdivresult.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int divisonResult(vector<int>& nums, int div) {
+        int cnt = 0;
+        for(int &n : nums) {
+            cnt += ceil(n / (double)div);
+        }
+        return cnt;
+    }
+    
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int left = 1, right = *max_element(nums.begin(), nums.end());
+        
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if(divisonResult(nums, mid) <= threshold) right = mid;
+            else left = mid + 1;
+        }
+        
+        return left;
+    }
+};
