014769330bc842f274d55bd0150e1bf4c57f0422
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-27 14:53:33 +0530

    Create minofverticestoreachallnodes.cpp

---
+++ Programs/Graph/minofverticestoreachallnodes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<bool> in_degree(n, false);
+        
+        for(auto edge : edges) in_degree[edge[1]] = true;
+        
+        vector<int> ans;
+        for(int i=0; i<n; ++i) {
+            if(in_degree[i] == false) ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
