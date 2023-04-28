19b6544e53866833b4b656852bc940301393378e
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-28 13:12:44 +0530

    Create repeatednaseq.cpp

---
+++ Programs/Tutorial3/repeatednaseq.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n = s.length();
+        vector<string> ans;
+        
+        if(n <= 10) return ans;
+        
+        unordered_map<string, int> umap;
+        
+        umap[s.substr(0, 10)]++;
+        
+        for(int i=10; i<n; ++i) {
+            umap[s.substr(i - 9, 10)]++;
+        }
+        
+        for(auto pair : umap) {
+            if(pair.second > 1) ans.push_back(pair.first);
+        }
+        
+        return ans;
+    }
+};
