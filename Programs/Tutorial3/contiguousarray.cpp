2fb8e8e0da22b4244dbed279e3c388930690c91c
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-04-28 12:50:21 +0530

    Create contiguousarray.cpp

---
+++ Programs/Tutorial3/contiguousarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, int> umap;
+        umap[0] = -1;
+        int ans = 0;
+        int cnt = 0;
+        
+        for(int i=0; i<n; ++i) {
+            cnt += nums[i] == 1 ? 1 : -1;
+            if(umap.find(cnt) != umap.end()) {
+                int diff = i - umap[cnt];
+                ans = max(ans, diff);
+                continue;
+            }
+            
+            umap[cnt] = i;
+        }
+        
+        return ans;
+    }
+};
